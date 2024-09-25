6b3cbde4c2c3a10be8c406439d3a90ea65aab172
Gabszzz05 <gabszzz005@gmail.com>
2024-09-25 16:08:26 -0500

    Creacion proyecto backend

---
+++ src/main/java/pe/edu/cibertec/Autos_BackEnd/AutosBackEndApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Autos_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/Autos_BackEnd/AutosBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Autos_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
